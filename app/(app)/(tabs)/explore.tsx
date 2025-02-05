03a6a930d362bb09e988e5795d9fea80a69320f7
Patrick Arminio <patrick.arminio@gmail.com>
2025-02-05 23:14:49 +0000

    Schedule is ready!

--- app/(app)/(tabs)/explore.tsx
+++ app/(app)/(tabs)/explore.tsx
@@ -1,18 +1,25 @@
 import { clsx } from 'clsx';
-import {
-  StyleSheet,
-  View,
-  Platform,
-  Text,
-  ScrollView,
-  SafeAreaView,
-} from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { FlashList } from '@shopify/flash-list';
 
-import { graphql, type ResultOf } from '@/graphql';
+import { graphql, readFragment, type ResultOf } from '@/graphql';
 import { useSuspenseQuery } from '@apollo/client';
 
-const SCHEDULE_QUERY = graphql(`
+const ITEM_FRAGMENT = graphql(`
+  fragment Item on ScheduleItem {
+    id
+    duration
+    start
+    end
+    title
+    rooms {
+      id
+    }
+  }
+`);
+
+const SCHEDULE_QUERY = graphql(
+  `
 query Schedule {
   conference(code: "pycon2024") {
     days {
@@ -25,74 +32,187 @@ query Schedule {
       day
       slots {
         hour
+        duration
         items {
-          id
-          duration
-          start
-          end
-          title
-          rooms {
-            id
-          }
+          ...Item
         }
       }
     }
+    }
   }
-}  
-`);
+`,
+  [ITEM_FRAGMENT],
+);
 
-function dateToMinutes(dateString: string) {
-  const date = new Date(dateString);
+type Item = ResultOf<typeof ITEM_FRAGMENT>;
+type ItemWithDuration = Omit<Item, 'duration'> & { duration: number };
 
-  return date.getHours() * 60 + date.getMinutes();
-}
+type Slot =
+  | {
+      type: 'break';
+      start: string;
+      title: string;
+    }
+  | {
+      type: 'room-change';
+      start: string;
+    }
+  | {
+      type: 'sessions';
+      start: string;
+      duration: number;
+      sessions: Array<ItemWithDuration>;
+    };
+
+type ScheduleSession = {
+  id: string;
+  width: number;
+  left: number;
+  session: Item;
+};
+
+const getSlotSize = (slot: Slot) => {
+  switch (slot.type) {
+    case 'break':
+      return 150;
+    case 'room-change':
+      return 10;
+    case 'sessions':
+      return 200;
+  }
+};
+
+const hourFromDatetime = (
+  datetime: string,
+): `${number}:${number}:${number}` => {
+  const date = new Date(datetime);
+
+  const [hours, minutes, seconds] = [
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+  ];
+
+  return [hours, minutes, seconds]
+    .map((part) => part.toString().padStart(2, '0'))
+    .join(':') as `${number}:${number}:${number}`;
+};
 
 function getDailySchedule(data: ResultOf<typeof SCHEDULE_QUERY>, day: number) {
-  // TODO: get min start time
   const { slots, rooms: dayRooms } = data.conference.days[day];
 
-  type Item = (typeof data.conference.days)[0]['slots'][0]['items'][0];
+  const daySlots: { [key: string]: Slot } = {};
 
-  let minStart = dateToMinutes(slots[0].items[0].start);
-  let maxEnd = dateToMinutes(slots[0].items[0].end);
+  const dayItems: Array<ItemWithDuration> = [];
 
-  const sessionsByRoomId = slots.reduce(
-    (acc, slot) => {
-      for (const originalItem of slot.items) {
-        const item = { ...originalItem };
+  for (const slot of slots) {
+    const items = readFragment(ITEM_FRAGMENT, slot.items).map((item) => ({
+      ...item,
+      duration:
+        item.duration ??
+        (new Date(item.end).getTime() - new Date(item.start).getTime()) / 60000,
+    }));
 
-        minStart = Math.min(minStart, dateToMinutes(item.start));
+    dayItems.push(...items);
 
-        if (!item.duration) {
-          // TODO: diff in minutes based on start and end instead of converting to minutes first
-          item.duration = dateToMinutes(item.end) - dateToMinutes(item.start);
-
-          console.log(
-            'No duration for item',
-            dateToMinutes(item.start),
-            dateToMinutes(item.end),
-          );
-        }
-
-        maxEnd = Math.max(maxEnd, dateToMinutes(item.start) + item.duration);
-
-        for (const room of item.rooms) {
-          if (!acc[room.id]) {
-            acc[room.id] = [];
-          }
-          acc[room.id].push(item);
-        }
+    if (slot.items.length === 1) {
+      if (items[0].title.toLowerCase().includes('room change')) {
+        daySlots[slot.hour] = {
+          type: 'room-change',
+          start: slot.hour,
+        };
+      } else {
+        daySlots[slot.hour] = {
+          type: 'break',
+          start: slot.hour,
+          title: items[0].title,
+        };
       }
+    } else {
+      const existingSlot = daySlots[slot.hour];
+
+      if (existingSlot && existingSlot.type !== 'sessions') {
+        throw new Error('Slot is not a session');
+      }
+
+      const existingSessions = existingSlot?.sessions ?? [];
+
+      const sessions = [...existingSessions, ...items];
+
+      daySlots[slot.hour] = {
+        type: 'sessions',
+        start: slot.hour,
+        duration: slot.duration,
+        sessions,
+      };
+    }
+  }
+
+  const getLeft = (item: Item) => {
+    let left = 0;
+
+    const itemStartHour = hourFromDatetime(item.start);
+
+    for (const slot of Object.values(daySlots)) {
+      if (slot.start === itemStartHour) {
+        break;
+      }
+
+      left += getSlotSize(slot);
+    }
+
+    return left;
+  };
+
+  const getWidth = (item: ItemWithDuration, slot: Slot) => {
+    if (slot.type === 'sessions') {
+      const pixelsPerMinute = getSlotSize(slot) / slot.duration;
+
+      return item.duration * pixelsPerMinute;
+    }
+
+    return getSlotSize(slot);
+  };
+
+  let scheduleSize = 0;
+
+  const sessionsByRoom = dayItems.reduce(
+    (acc, item) => {
+      const slot = daySlots[hourFromDatetime(item.start)];
+
+      if (!slot) {
+        throw new Error('Slot not found');
+      }
+
+      const width = getWidth(item, slot);
+      const left = getLeft(item);
+
+      scheduleSize = Math.max(scheduleSize, left + width);
+
+      const scheduleSession = {
+        id: item.id,
+        session: item,
+        width,
+        left,
+      };
+
+      for (const room of item.rooms) {
+        if (!acc[room.id]) {
+          acc[room.id] = [];
+        }
+        acc[room.id].push(scheduleSession);
+      }
+
       return acc;
     },
-    {} as Record<string, Array<Item>>,
+    {} as Record<string, Array<ScheduleSession>>,
   );
 
   const rooms = dayRooms.flatMap((room) => {
-    return [room.name, sessionsByRoomId[room.id]];
+    return [room.name, sessionsByRoom[room.id]];
   });
 
-  const stickyHeaderIndices = rooms
+  const roomTitleIndices = rooms
     .map((item, index) => {
       if (typeof item === 'string') {
         return index;
@@ -101,9 +221,7 @@ function getDailySchedule(data: ResultOf<typeof SCHEDULE_QUERY>, day: number) {
     })
     .filter((item) => item !== null) as number[];
 
-  const duration = maxEnd - minStart;
-
-  return { rooms, stickyHeaderIndices, duration };
+  return { rooms, roomTitleIndices, scheduleSize };
 }
 
 export default function ContactsFlashList() {
@@ -111,18 +229,14 @@ export default function ContactsFlashList() {
 
   const schedule = getDailySchedule(data, 1);
 
-  const minutesToPixel = (minutes: number) => {
-    return minutes * 8;
-  };
-
   return (
     <ScrollView horizontal className="flex-1 bg-red-200 pb-24">
       <View
         className="flex-1 bg-blue-200"
-        style={{ width: minutesToPixel(schedule.duration) }}
+        style={{ width: schedule.scheduleSize }}
       >
         <FlashList
-          className={clsx('flex-1 bg-blue-200')}
+          className={clsx('flex-1 bg-[#FAF5F3]')}
           data={schedule.rooms}
           renderItem={({ item, target, index }) => {
             if (typeof item === 'string') {
@@ -141,31 +255,57 @@ export default function ContactsFlashList() {
             }
 
             return (
-              <View className="flex-row h-36 border-b-4">
-                {item.map((session, index) => {
+              <View className="flex-row h-36 border-b-4 relative">
+                {item.map(({ session, width, left }, index) => {
                   const start = new Date(session.start);
                   const formattedStart = start.toLocaleTimeString('en-GB', {
                     hour: '2-digit',
                     minute: '2-digit',
                   });
+                  const end = new Date(session.end);
+                  const formattedEnd = end.toLocaleTimeString('en-GB', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                  });
+
+                  const isRoomChange = session.title
+                    .toLowerCase()
+                    .includes('room change');
 
                   return (
                     <View
-                      className="border-r-4 h-100 p-4"
+                      className={clsx('border-l-4 h-full', {
+                        'p-4 bg-[#FCE8DE]': !isRoomChange,
+                      })}
                       key={session.id}
-                      style={{ width: minutesToPixel(session.duration) }}
+                      style={{
+                        width,
+                        left,
+                        position: 'absolute',
+                        transform: [{ translateX: -4 }],
+                      }}
                     >
-                      <Text className="font-sans-semibold">
-                        {session.title}
-                      </Text>
-                      <Text className="font-sans">{formattedStart}</Text>
+                      {isRoomChange ? null : (
+                        <View>
+                          <Text
+                            className="font-sans-semibold"
+                            numberOfLines={2}
+                          >
+                            {session.title}
+                          </Text>
+                          <Text className="font-sans">
+                            {formattedStart} - {formattedEnd}
+                          </Text>
+                        </View>
+                      )}
+                      <View className="w-1 bg-black absolute -right-1 top-0 bottom-0" />
                     </View>
                   );
                 })}
               </View>
             );
           }}
-          stickyHeaderIndices={schedule.stickyHeaderIndices}
+          // stickyHeaderIndices={schedule.roomTitleIndices}
           getItemType={(item) => {
             return typeof item === 'string' ? 'sectionHeader' : 'row';
           }}
