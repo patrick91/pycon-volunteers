5794d6de1ab0955788396ef7631eab1168044011
Patrick Arminio <patrick.arminio@gmail.com>
2025-04-21 15:33:39 +0100

    Better toggle

--- app/(auth)/(tabs)/schedule/_layout.tsx
+++ app/(auth)/(tabs)/schedule/_layout.tsx
@@ -1,11 +1,6 @@
 import { Stack } from 'expo-router';
-import { DaySelector } from '../../../components/day-selector';
-import { View, TouchableOpacity, Text } from 'react-native';
-import { useState } from 'react';
 
 export default function ScheduleLayout() {
-  const [viewMode, setViewMode] = useState<'grid' | 'list'>('grid');
-
   return (
     <Stack>
       <Stack.Screen
--- app/(auth)/(tabs)/schedule/index.tsx
+++ app/(auth)/(tabs)/schedule/index.tsx
@@ -5,9 +5,32 @@ import { Stack } from 'expo-router';
 import { DaySelector } from '@/app/components/day-selector';
 import { useState } from 'react';
 import { useSchedule } from '@/hooks/use-schedule';
+import Feather from '@expo/vector-icons/Feather';
+
+const ViewModeSelector = ({
+  viewMode,
+  onViewModeChange,
+}: {
+  viewMode: 'grid' | 'list';
+  onViewModeChange: (viewMode: 'grid' | 'list') => void;
+}) => {
+  if (viewMode === 'grid') {
+    return (
+      <TouchableOpacity onPress={() => onViewModeChange('list')}>
+        <Feather name="list" size={24} color="black" />
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <TouchableOpacity onPress={() => onViewModeChange('grid')}>
+      <Feather name="grid" size={24} color="black" />
+    </TouchableOpacity>
+  );
+};
 
 export default function SchedulePage() {
-  const [viewMode, setViewMode] = useState<'grid' | 'list'>('grid');
+  const [viewMode, setViewMode] = useState<'grid' | 'list'>('list');
 
   const [day, setDay] = useState('2025-05-28');
 
@@ -30,36 +53,10 @@ export default function SchedulePage() {
             </View>
           ),
           headerRight: () => (
-            <View className="flex-row mr-4">
-              <TouchableOpacity
-                className={`px-3 py-1 rounded-l ${
-                  viewMode === 'grid' ? 'bg-blue-500' : 'bg-gray-200'
-                }`}
-                onPress={() => setViewMode('grid')}
-              >
-                <Text
-                  className={`font-medium ${
-                    viewMode === 'grid' ? 'text-white' : 'text-gray-700'
-                  }`}
-                >
-                  Grid
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                className={`px-3 py-1 rounded-r ${
-                  viewMode === 'list' ? 'bg-blue-500' : 'bg-gray-200'
-                }`}
-                onPress={() => setViewMode('list')}
-              >
-                <Text
-                  className={`font-medium ${
-                    viewMode === 'list' ? 'text-white' : 'text-gray-700'
-                  }`}
-                >
-                  List
-                </Text>
-              </TouchableOpacity>
-            </View>
+            <ViewModeSelector
+              onViewModeChange={setViewMode}
+              viewMode={viewMode}
+            />
           ),
         }}
       />
