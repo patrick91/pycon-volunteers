9cad49de2a769860d0eb24ee1862e6f449505c98
Patrick Arminio <patrick.arminio@gmail.com>
2025-04-21 23:07:32 +0100

    Basic profile screen

--- app/(auth)/(tabs)/profile.tsx
+++ app/(auth)/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 import { graphql, readFragment, type FragmentOf } from '@/graphql';
 import { useSuspenseQuery } from '@apollo/client';
 import { useSession } from '@/context/auth';
@@ -8,6 +8,7 @@ export const USER_PROFILE_FRAGMENT = graphql(`
   fragment UserProfile on User {
     id
     email
+    fullName
     conferenceRoles(conferenceCode: "pycon2025")
   }
 `);
@@ -26,25 +27,42 @@ const USER_PROFILE_QUERY = graphql(
 const ProfileInfo = ({
   data,
 }: { data: FragmentOf<typeof USER_PROFILE_FRAGMENT> }) => {
-  const { email, conferenceRoles } = readFragment(USER_PROFILE_FRAGMENT, data);
+  const { signOut } = useSession();
+
+  const { fullName, email, conferenceRoles } = readFragment(
+    USER_PROFILE_FRAGMENT,
+    data,
+  );
 
   return (
-    <View>
-      <Text>{email}</Text>
-      <Text>{conferenceRoles.join(', ')}</Text>
+    <View className="p-4">
+      <Text className="text-2xl font-bold">Hello {fullName}</Text>
+
+      <View className="flex gap-2 mt-4">
+        <Text className="font-bold">Email</Text>
+        <Text>{email}</Text>
+      </View>
+
+      <View className="flex gap-2 mt-4">
+        <Text className="font-bold">Conference Roles</Text>
+        <Text>
+          {conferenceRoles.length > 0 ? conferenceRoles.join(', ') : 'None'}
+        </Text>
+      </View>
+
+      <Button className="mt-4" onPress={() => signOut()}>
+        Sign Out
+      </Button>
     </View>
   );
 };
 
 export default function Profile() {
   const { data } = useSuspenseQuery(USER_PROFILE_QUERY);
-  const { signOut } = useSession();
 
   return (
-    <View className="flex-1 bg-white">
-      <Text>Profile</Text>
+    <SafeAreaView className="flex-1 bg-white">
       <ProfileInfo data={data.me} />
-      <Button onPress={() => signOut()}>Sign Out</Button>
-    </View>
+    </SafeAreaView>
   );
 }
